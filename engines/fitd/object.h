5ec16f830a9dbe90a3fa3ed6d16ad2e80ce5f35e
Einar Johan T. Sømåen <einarjohants@gmail.com>
2011-12-08 20:00:04 +0100

    FITD: Added Residual-GPLv2-notice to FITD-files

--- engines/fitd/object.h
+++ engines/fitd/object.h
@@ -1,3 +1,24 @@
+/* Residual - A 3D game interpreter
+ *
+ * Residual is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #ifndef _OBJECT_H_
 #define _OBJECT_H_
 int copyObjectToActor(int flag2, int var1, int foundName, short int flag, int x, int y, int z, int stage, int room, int alpha, int beta, int gamma, int var2, int var3, int var4, int var5);
